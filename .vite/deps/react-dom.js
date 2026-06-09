b4a1c6f4253f6588547c9794f3539e842279d71d
Alfiya nasrin <alfiya.nazrinva@gmail.com>
2026-06-09 17:16:06 +0530

    chore: remove node_modules from git

--- .vite/deps/react-dom.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-EQ63JTKP.js";
-import "./chunk-DXUXPUGD.js";
-export default require_react_dom();
-//# sourceMappingURL=react-dom.js.map
--- .vite/deps/react_jsx-runtime.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-PTUVVZOA.js";
-import "./chunk-DXUXPUGD.js";
-export default require_jsx_runtime();
-//# sourceMappingURL=react_jsx-runtime.js.map
